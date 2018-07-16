319cd13ddd5d09f154952341374849c4cc60685d
Lamprini Vasilaki <lamprinivasilaki@gmail.com>
2018-07-16 15:25:47 +0300

    add dump_with_schema() on sith controller

--- poc/controllers/siths.py
+++ poc/controllers/siths.py
@@ -5,6 +5,16 @@ from flask_restplus import Resource
 from marshmallow import Schema, fields
 
 
+def dump_with_schema(schema: Schema):
+    def decorator(func):
+        def wrapper(*args, **kwargs) -> str:
+            response = func(*args, **kwargs)
+
+            return jsonify(response)
+        return wrapper
+    return decorator
+
+
 class SithSchema(Schema):
     id = fields.Number()
     name = fields.String()
@@ -22,13 +32,15 @@ ns = api.namespace('sith', description='Operations on Sith Lords')
 
 @ns.route('')
 class Sith(Resource):
+
+    @dump_with_schema(SithSchema())
     def get(self):
         sith_id = request.args.get("id")
         movie = request.args.get("movie")
 
         for sith in sith_lords:
             if sith["id"] == int(sith_id) and sith["movie"] == movie:
-                return jsonify({'sith': sith})
+                return sith
 
-        return jsonify({'siths': sith_lords})
+        return sith_lords
 
